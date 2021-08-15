56fa6247bc6d20b5439cc4e7b724e929d4b2e9ae
vvoshipt <43911520+vvoshipt@users.noreply.github.com>
2021-08-15 11:11:34 -0700

    8_15 solution

---
+++ 2021_08/8_15.go
@@ -0,0 +1,43 @@
+package main
+
+// Given an array of integers, return a new array such that each element at
+//index i of the new array is the product of all the numbers in the original
+//array except the one at i.
+//
+//For example, if our input was [1, 2, 3, 4, 5], the expected output would
+//be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output
+//would be [2, 3, 6].
+//
+//Follow-up: what if you can't use division?
+
+func GetProductOfSliceWithDivision(sample []int) []int {
+	// Get Products of all Elements then divide to get result
+	//Solve Time (15 minutes)
+
+	if len(sample) < 2 {
+		return []int{}
+	}
+
+	response := make([]int, len(sample))
+	Product := GetProductOfAllElements(sample)
+
+	for i, factor := range sample {
+		response[i] = Product / factor
+	}
+
+	return response
+}
+
+func GetProductOfAllElements(sample []int) int {
+	if len(sample) < 1 {
+		return -1
+	}
+
+	ProductResult := 1
+	for _, factor := range sample {
+		ProductResult = ProductResult * factor
+	}
+	return ProductResult
+}
+
+
---
+++ 2021_08/8_15_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetProductsOfSlice(t *testing.T) {
+	type TestCase struct {
+		Name           string
+		Input          []int
+		ExpectedResult []int
+	}
+
+	Cases := []TestCase{
+		{
+			Name:           "Single Element",
+			Input:          []int{3},
+			ExpectedResult: []int{},
+		},
+		{
+			Name:           "No Elements",
+			Input:          []int{},
+			ExpectedResult: []int{},
+		},
+		{
+			Name:           "2 Elements",
+			Input:          []int{1, 2},
+			ExpectedResult: []int{2, 1},
+		},
+		{
+			Name:           "3 Elements",
+			Input:          []int{3, 2, 1},
+			ExpectedResult: []int{2, 3, 6},
+		},
+		{
+			Name:           "Example from Problem",
+			Input:          []int{1, 2, 3, 4, 5},
+			ExpectedResult: []int{120, 60, 40, 30, 24},
+		},
+	}
+
+	for _, tt := range Cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			result := GetProductOfSliceWithDivision(tt.Input)
+			assert.Equal(t, tt.ExpectedResult, result)
+		})
+	}
+}
+
+func TestGetProductOfAllElements(t *testing.T) {
+	type TestCase struct {
+		Name           string
+		Input          []int
+		ExpectedResult int
+	}
+
+	Cases := []TestCase{
+		{
+			Name:           "No Element",
+			Input:          []int{},
+			ExpectedResult: -1,
+		},
+		{
+			Name:           "One Element",
+			Input:          []int{2},
+			ExpectedResult: 2,
+		},
+		{
+			Name:           "3 Element",
+			Input:          []int{1, 2, 3},
+			ExpectedResult: 6,
+		},
+	}
+
+	for _, tt := range Cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			result := GetProductOfAllElements(tt.Input)
+			assert.Equal(t, tt.ExpectedResult, result)
+		})
+	}
+}
