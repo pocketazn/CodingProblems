ff47518101e2ba2c17a5bb1f46681b390bc5a71c
vvoshipt <43911520+vvoshipt@users.noreply.github.com>
2021-08-18 16:59:33 -0700

    8_18 code problem with solution

---
+++ 2021_08/8_18.go
@@ -0,0 +1,35 @@
+package _2021_08
+
+// cons(a, b) constructs a pair, and car(pair) and cdr(pair)
+//returns the first and last element of that pair. For example,
+//car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+//
+//Given this implementation of cons:
+//
+//def cons(a, b):
+//    def pair(f):
+//        return f(a, b)
+//    return pair
+//Implement car and cdr.
+
+// solve time. 3 Mins
+
+type Pair struct {
+	First int
+	Second int
+}
+
+func Cons(f int, s int) Pair {
+	return Pair{
+		First: f,
+		Second: s,
+	}
+}
+
+func Car(p Pair) int {
+	return p.First
+}
+
+func Cdr(p Pair) int {
+	return p.Second
+}
---
+++ 2021_08/8_18_test.go
@@ -0,0 +1,20 @@
+package _2021_08
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCarConTest(t *testing.T) {
+	t.Run("Example Problem A", func(t *testing.T) {
+		result := Car(Cons(3,4))
+		assert.Equal(t, 3, result)
+	})
+}
+
+func TestCdrConTest(t *testing.T) {
+	t.Run("Example Problem B", func(t *testing.T) {
+		result := Cdr(Cons(3,4))
+		assert.Equal(t, 4, result)
+	})
+}
